055e9ef6deaa51169811377001742a041cb82434
Cas <vdveldecasper@gmail.com>
2024-05-31 03:21:43 +0200

    update doc site

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Documentation site loaded');
 });
+
+
+function searchDocumentation() {
+    var input = document.getElementById("searchInput").value.toLowerCase();
+    var scripts = document.querySelectorAll(".script-buttons a");
+
+    scripts.forEach(function(script) {
+        var scriptName = script.innerText.toLowerCase();
+        var scriptSection = script.parentElement.parentElement;
+
+        if (scriptName.indexOf(input) > -1) {
+            scriptSection.style.display = "";
+        } else {
+            scriptSection.style.display = "none";
+        }
+    });
+}
